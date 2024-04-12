c9afb34f046f0520d46e541f290eabd59dba4cdb
Gonzalo <gverap92@gmail.com>
2024-04-12 11:00:14 -0400

    Added update Finished to UserTask

--- LegalTrace.BLL/Models/UserTaskDTO/UserTaskEditDTO.cs
+++ LegalTrace.BLL/Models/UserTaskDTO/UserTaskEditDTO.cs
@@ -5,9 +5,10 @@
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        public DateTime DueDate { get; set; }
+        public DateTime? DueDate { get; set; }
         public string Type { get; set; } = string.Empty;
         public int UserId { get; set; }
         public int ClientId { get; set; }
+        public bool? Finished { get; set; }
     }
 }
